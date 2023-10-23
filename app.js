b3b3dca337803856254b75f0907c2f15bca22529
unknown <rodolfcs@gmail.com>
2023-10-23 19:48:26 -0300

    Progresso

---
+++ app.js
@@ -0,0 +1,43 @@
+/*
+Progresso semanal:
+
+1 - Criei uma nova branch e subi pra la uns repositorios.
+2 - Criei alguns testes de Ruby.
+3 - Entendi  muito bem o curso de Respec do SDPs
+4 - Estou entendendo melhor o react
+5 - Fiz mais de 5 horas no wakatime, sabado, domingo e segunda respectivamente. 
+
+*/
+
+/*
+ 
+Marque como feito após e apague:
+
+Pra essa semana:
+1 - Concluir o curso Rspec testes
+2 - React => Fazer as configuraçoes para o uso do react
+3 - Fazer 30 minutos por dia de revisão JS (Muito importante)
+4 - Dar mais um 'pega' no meu portifolio
+5 - Subir pro gitHub todos os dias(Sem falhar)
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
