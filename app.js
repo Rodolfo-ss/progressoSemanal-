06fcdd0401da98699596f6ffcdb34438eb466ad8
Rodolfo  Campos <111758185+Rodolfo-ss@users.noreply.github.com>
2023-11-01 21:23:05 -0300

    Update app.js

--- app.js
+++ app.js
@@ -14,13 +14,14 @@ Novos objetivos:
 Marque como feito após e apague:
 
 Pra essa semana: Em ordem de importancia
-2 - React => Fazer as configuraçoes para o uso do react(amanha)
-3 - Fazer 30 minutos por dia de revisão JS (Muito importante)
+Pra fazer => 2 - React => Fazer as configuraçoes para o uso do react
+
+3 - Fazer 30 minutos por dia de revisão JS CHECKED
 5 - Subir pro gitHub todos os dias(Sem falhar)
 
 Meta para todas semanas: 
 
-1 - Wakatime 7 Horas por dia(no minimo)check hoje
+1 - Wakatime 7 Horas por dia(no minimo)checkd hoje
 2 - React, dar uma boa atenção no react
 3 - SDPs, trazer o metodo do SDPs pra sua realidade.
 5 - Apagar sempre que concluir uma meta
