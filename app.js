802439ee3cb6c424aea67aa5fd02fe7b3b10eabe
Rodolfo  Campos <111758185+Rodolfo-ss@users.noreply.github.com>
2023-11-03 20:43:27 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 /*
-Novos objetivos:
+
+Caixa de entrada:
+A fazer => 
 
 1 - Missao vaga
 2 - Revisao CJRM
-3 - Crud
 4 - Pull request e branchs 
 5 - Quando for fazer portifolio faça tudo de uma vez
 
